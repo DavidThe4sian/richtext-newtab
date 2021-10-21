5519fcc5db25bb9e9bb69ed59fea9f878af43ced
David <maxxx818@umn.edu>
2021-10-21 18:26:34 -0500

    adding stuff

---
+++ background.js
@@ -0,0 +1,20 @@
+// background.js
+
+let color = '#3aa757';
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
+});
+
+var urlRegex = "";
+chrome.tabs.onRemoved.addListener(function(tabId, info) {
+    chrome.tabs.get(tabId, function(tab) {
+      console.log(tabId);
+        if (urlRegex.test(tab.url)) {
+            /* The tab with ID `tabId`, with a web-page in the
+             * `google.com` domain, is being closed. Let's do something... */
+            console.log('hello');
+        }
+    });
+});
